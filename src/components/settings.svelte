7b05b099b5e38de9df31762bc0f147705cda840c
ylahssini <ylahssini@gmail.com>
2022-07-07 02:12:27 +0100

    style the game in progress

---
+++ src/components/settings.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+    import marvel from '../assets/images/marvel.png';
+    import dc from '../assets/images/dc.png';
+    import starwars from '../assets/images/starwars.png';
+    import nintendo from '../assets/images/nintendo.png';
+
+    let themes = [
+        { value: 'marvel', label: 'Marvel', image: marvel },
+        { value: 'dc', label: 'DC', image: dc },
+        { value: 'starwars', label: 'Star wars', image: starwars },
+        { value: 'nintendo', label: 'Nintendo', image: nintendo },
+    ]
+</script>
+
+<div>
+    <p>Choose the theme of the game</p>
+    <ul>
+        {#each themes as theme}
+            <li>
+                <input type="radio" name="theme" value={theme.value} id={theme.value}>
+                <label for="{theme.value}">
+                    <img src={theme.image} alt="" />
+                    <span>{theme.label}</span>
+                </label>
+            </li>
+        {/each}
+    </ul>
+
+    <p>Choose the size of grid</p>
+    <ol>
+        <li><label><input type="radio" name="size" value="4"> <span>4x4</span></label></li>
+        <li><label><input type="radio" name="size" value="6"> <span>6x6</span></label></li>
+    </ol>
+</div>
+
+<style lang="scss">
+    input {
+        display: none;
+    }
+
+    p {
+        font-size: 1.15rem;
+        padding: 0 0 2rem;
+        text-align: center;
+    }
+
+    ul {
+        margin: 0 0 3rem;
+        display: flex;
+        align-items: center;
+        gap: 1.5rem;
+
+        > li {
+            > input {
+                display: none;
+
+                &:checked + label {
+                    opacity: 1;
+                    transform: translateY(-.65rem);
+                    box-shadow: 0 0 40px -15px rgb(228, 82, 99);
+                }
+            }
+
+            > label {
+                box-shadow: 0 0 0 rgba(0, 0, 0, 0);
+                background-color: #fff;
+                text-align: center;
+                border-radius: 9px;
+                font-size: 1.35rem;
+                overflow: hidden;
+                opacity: 0.4;
+                cursor: pointer;
+                display: block;
+                transform: translateY(0);
+                transition: box-shadow .2s ease-out, opacity .3s ease-out, transform .3s ease-out;
+
+                &:hover {
+                    box-shadow: 0 0 25px 2px rgba(0, 0, 0, 0.11);
+                    opacity: 1;
+                }
+
+                > span {
+                    display: block;
+                    padding: 1rem 0;
+                }
+            }
+        }
+    }
+
+    ol {
+        margin: 0 auto 2rem;
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+        width: fit-content;
+
+        label {
+            box-shadow: 0 0 0 rgba(0, 0, 0, 0);
+            background-color: #fff;
+            text-align: center;
+            border-radius: 9px;
+            font-size: 1.35rem;
+            padding: 1rem;
+            overflow: hidden;
+            cursor: pointer;
+            display: block;
+            transition: box-shadow .2s ease-out;
+
+            > input {
+                display: none;
+            }
+        }
+    }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,46 +1,78 @@
 <script lang="ts">
-    import '../assets/styles/app.css';
+	import '../assets/styles/app.css';
 </script>
 
 <svelte:head>
-    <title>Memory game | created by Youssef Lahssini</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css2?family=Atma:wght@300;700&display=swap" rel="stylesheet" /> 
+	<title>Memory game | created by Youssef Lahssini</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Baloo+Paaji+2:wght@400;600&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 <main>
-    <header>
-        <h1>Memory Game</h1>
-    </header>
-    <section>
-        <slot></slot>
-    </section>
-    <footer>
-        <p>Created by <a href="https://ylahssini.vercel.app" target="_blank">Youssef Lahssini</a></p>
-        <p>Code source in <a href="https://github.com/ylahssini/memory-game" target="_blank">Github</a></p>
-    </footer>
+	<header>
+		<h1>Memory<br />Game</h1>
+	</header>
+	<section>
+		<slot />
+	</section>
+	<footer>
+		<p>
+			&copy; All rights reserved. Created by <a href="https://ylahssini.vercel.app" target="_blank"
+				>Youssef Lahssini</a
+			>
+		</p>
+		<p>
+			Code source in <a href="https://github.com/ylahssini/memory-game" target="_blank">Github</a>
+		</p>
+	</footer>
 </main>
 
 <style lang="scss">
-    main {
-        display: grid;
-        grid-template-columns: 1fr;
-        gap: 0;
-        grid-template-rows: 0.2fr 2.7fr 0.1fr;
-        grid-template-areas: "header" "content" "footer";
-        height: 100vh;
-    }
+	main {
+		display: grid;
+		grid-template-columns: 1fr;
+		gap: 1rem;
+		grid-template-rows: 0.2fr 2.7fr 0.1fr;
+		grid-template-areas: 'header' 'content' 'footer';
+		width: 800px;
+	}
 
-    header {
-        grid-area: 'header';
-    }
+	header {
+		grid-area: header;
+		display: flex;
+		align-items: flex-end;
+		justify-content: space-between;
+        padding: 0 0 0.7rem;
+        border-bottom: 3px solid var(--dark);
+	}
 
-    section {
-        grid-area: 'content';
-    }
+	h1 {
+		font-size: 3rem;
+		font-weight: 700;
+		letter-spacing: -3px;
+		line-height: 2.1rem;
+	}
 
-    footer {
-        grid-area: 'footer';
-    }
+	section {
+		border-radius: 8px;
+		grid-area: content;
+		width: 100%;
+		height: 70vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+	}
+
+	footer {
+		grid-area: footer;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+        padding-top: 0.7rem;
+        border-top: 1px solid var(--dark);
+	}
 </style>
