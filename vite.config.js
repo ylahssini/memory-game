4fc0323be89732bbf07d198cad5bdc4a049529c6
ylahssini <ylahssini@gmail.com>
2022-07-23 13:24:37 +0100

    add build

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-
-/** @type {import('vite').UserConfig} */
-const config = {
-    base: '/memory-game/',
-    plugins: [sveltekit()],
-};
-
-export default config;
